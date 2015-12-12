7ae7af60d8b338021123fb4904a54943e81a7137
o0milanzoric0o <o0milanzoric0o@gmail.com>
2015-12-12 23:43:16 +0100

    Commit complete project.

---
+++ app/src/main/java/rs/direktnoizbaste/dizb/app/AppConfig.java
@@ -0,0 +1,12 @@
+package rs.direktnoizbaste.dizb.app;
+
+/**
+ * Created by milan on 12/12/2015.
+ */
+public class AppConfig {
+    // Server login url
+    public static String URL_LOGIN = "http://192.168.1.100/login_register_api/";
+
+    // Server register url
+    public static String URL_REGISTER = "http://192.168.1.100/login_register_api/";
+}
