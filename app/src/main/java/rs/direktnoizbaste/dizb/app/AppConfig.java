ff5e9b1ed1c18dfb98a421a3615ecb2ecc951131
Milan <o0milanzoric0o@gmail.com>
2016-01-28 22:05:59 +0100

    Updated GET links...

--- app/src/main/java/rs/direktnoizbaste/dizb/app/AppConfig.java
+++ app/src/main/java/rs/direktnoizbaste/dizb/app/AppConfig.java
@@ -7,7 +7,6 @@ public class AppConfig {
     // Server login url
     public static String URL_LOGIN_POST = "http://direktnoizbaste.rs/parametri.php";
     //"action=povuciPodatkeAndroidKorisnik&email=pera@gjsd.com&p=miki";
-
     public static String URL_LOGIN_GET = "http://direktnoizbaste.rs/parametri.php?action=povuciPodatkeAndroidKorisnik&tag=%1$s&email=%2$s&p=%3$s";
     // Server register url
     public static String URL_REGISTER_POST = "http://direktnoizbaste.rs/parametri.php";
@@ -17,5 +16,8 @@ public class AppConfig {
     public static String URL_SENSOR_LIST_GET = "http://direktnoizbaste.rs/parametri.php?action=povuciSenzorUid&id=%1$s";
     //http://direktnoizbaste.rs/parametri.php?action=povuciPodatkeSenzorId&string=5CCF7F747A7&id=1
     public static String URL_GRAPHS_DATA_GET = "http://direktnoizbaste.rs/parametri.php?action=povuciPodatkeSenzorId&id=%1$s&string=%2$s";
-
+    //http://direktnoizbaste.rs/parametri.php?action=obrisiSenzorId&string=pera&id=1
+    public static String URL_DEL_SENSOR_GET = "http://direktnoizbaste.rs/parametri.php?action=obrisiSenzorId&id=%1$s&string=%2$s";
+    //http://direktnoizbaste.rs/parametri.php?action=dodajSenzorId&string=pera&id=1&br=4
+    public static String URL_ADD_SENSOR_GET = "http://direktnoizbaste.rs/parametri.php?action=dodajSenzorId&id=%1$s&string=%2$s&br=%3$s";
 }
