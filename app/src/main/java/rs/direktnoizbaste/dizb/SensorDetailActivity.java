749426884bc0cdd8d7708051b1d075952a4f7e4f
nemanjamil <nemanjamil@gmail.com>
2018-05-26 20:09:00 +0200

    Milance, poceo sam da pravim SensorDetailActiviti
    dodao mi je u gradle  ->  implementation 'com.android.support.constraint:constraint-layout:1.1.0'

--- app/src/main/java/rs/direktnoizbaste/dizb/SensorDetailActivity.java
+++ app/src/main/java/rs/direktnoizbaste/dizb/SensorDetailActivity.java
@@ -1,6 +1,5 @@
 package rs.direktnoizbaste.dizb;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.android.volley.DefaultRetryPolicy;
@@ -20,50 +20,66 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 import rs.direktnoizbaste.dizb.app.AppConfig;
 import rs.direktnoizbaste.dizb.app.AppController;
 import rs.direktnoizbaste.dizb.app.SessionManager;
-import rs.direktnoizbaste.dizb.dialogs.ProgressDialogCustom;
+
 
 public class SensorDetailActivity extends AppCompatActivity {
 
-    private ProgressDialogCustom progressDialog;
     private SessionManager session;
+    String SensorMAC = null;
+    Integer KulturaId = null;
+    String userID = null;
+    JSONObject jsonObject;
+    JSONArray jsonArray;
+    ArrayList arraylist;
+
+    ListView listView;
+    ViewAdapterSensorDetail adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sensor_detail);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sensor_detail_two);
+       /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+        arraylist = new ArrayList<>();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        // lociramo GridView
+        listView = (ListView) findViewById(R.id.idListViewKategorije);
+
+        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
-        });
+        });*/
 
-        String SensorMAC = null;
-        Integer KulturaId = null;
+
         Bundle extras = getIntent().getExtras();
-        if(extras != null) {
+        if (extras != null) {
             SensorMAC = extras.getString("SensorMAC"); // retrieve the data using keyName
             KulturaId = extras.getInt("KulturaId");
         }
-        Toast.makeText(this, SensorMAC+" "+KulturaId, Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, SensorMAC + " " + KulturaId, Toast.LENGTH_SHORT).show();
 
         //setting progressDialog
-        progressDialog = new ProgressDialogCustom(this);
-        progressDialog.setCancelable(false);
+        /*progressDialog = new ProgressDialogCustom(this);
+        progressDialog.setCancelable(false);*/
 
         session = new SessionManager(getApplicationContext());
-        String userID = session.getUID();
+        userID = session.getUID();
         Log.d("testmiki", userID);
         Log.d("testmiki", "aaa");
         getResults(SensorMAC, KulturaId, userID);
@@ -71,44 +87,97 @@ public class SensorDetailActivity extends AppCompatActivity {
 
     }
 
-    private void getResults(final String sensorMAC, final Integer kulturaId, final String uid){
+    private void getResults(final String sensorMAC, final Integer kulturaId, final String uid) {
         // Tag used to cancel the request
         String tag_string_req = "req_login";
 
-        progressDialog.showDialog("Getting informations...");
-        String url = String.format(AppConfig.URL_LOGIN_GET, "login", sensorMAC, kulturaId,uid);
+        //progressDialog.showDialog("Getting informations...");
+        //String url = String.format(AppConfig.URL_LOGIN_GET, "login", sensorMAC, kulturaId, uid);
 
         StringRequest strReq = new StringRequest(Request.Method.POST,
                 AppConfig.URL_GETINFOPARAMETER_POST, new Response.Listener<String>() {
 
             @Override
             public void onResponse(String response) {
-                progressDialog.hideDialog();
+                //progressDialog.hideDialog();
 
                 try {
 
-                   Log.d("testmiki", "Usao u TRY");
-                   JSONObject jObj = new JSONObject(response);
-                   boolean success = jObj.getBoolean("success");
+                    Log.d("testmiki", "Usao u TRY");
+                    jsonObject = new JSONObject(response);
+                    boolean success = jsonObject.getBoolean("success");
 
                     if (success) {
 
-                        JSONArray jArr = jObj.getJSONArray("podaciSenzor");
+                        jsonArray = jsonObject.getJSONArray("podaciSenzor");
 
-                        for (int i = 0; i < jArr.length(); i++)
-                        {
-                            String ImeKulture = jArr.getJSONObject(i).getString("ImeKulture");
-                            Integer IdListaSenzora = jArr.getJSONObject(i).getInt("IdListaSenzora");
-                            Integer IdKulture = jArr.getJSONObject(i).getInt("IdKulture");
-                            Integer IdSenzorTip = jArr.getJSONObject(i).getInt("IdSenzorTip");
-                            String senzorTipIme = jArr.getJSONObject(i).getString("senzorTipIme");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+
+                            JSONObject c = jsonArray.getJSONObject(i);
+                            String ImeKulture = c.getString("ImeKulture");
+                            Integer IdListaSenzora = c.getInt("IdListaSenzora");
+                            Integer IdKulture = c.getInt("IdKulture");
+                            Integer IdSenzorTip = c.getInt("IdSenzorTip");
+                            String senzorTipIme = c.getString("senzorTipIme");
+
+                            Integer OdPodaciIdeal = c.getInt("OdPodaciIdeal");
+                            Integer DoPodaciIdeal = c.getInt("DoPodaciIdeal");
+                            Integer OdZutoIdeal = c.getInt("OdZutoIdeal");
+                            Integer DoZutoIdeal = c.getInt("DoZutoIdeal");
+
+
+                            HashMap<String, String> grupaPodataka = new HashMap<>();
+                            grupaPodataka.put("ImeKulture", ImeKulture);
+                            grupaPodataka.put("IdListaSenzora", String.valueOf(IdListaSenzora));
+                            grupaPodataka.put("IdSenzorTip", String.valueOf(IdSenzorTip));
+                            grupaPodataka.put("senzorTipIme", senzorTipIme);
+                            grupaPodataka.put("OdPodaciIdeal", String.valueOf(OdPodaciIdeal));
+                            grupaPodataka.put("DoPodaciIdeal", String.valueOf(DoPodaciIdeal));
+                            grupaPodataka.put("OdZutoIdeal", String.valueOf(OdZutoIdeal));
+                            grupaPodataka.put("DoZutoIdeal", String.valueOf(DoZutoIdeal));
+
+                            jsonArray = c.getJSONArray("podacizaSenzor");
+                            for (int y = 0; y < jsonArray.length(); y++) {
+
+
+
+                                JSONObject m = jsonArray.getJSONObject(y);
+                                //String vremeSenzor = m.getString("vremeSenzor");
+
+                                String vremeSenzor = m.getString("vremeSenzor");
+                                DateFormat df = new SimpleDateFormat("dd-MM-YYYY HH:mm:ss");
+                                try {
+                                    Date birthDate = df.parse(vremeSenzor);
+                                } catch (ParseException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                                Integer idSenzorIncr = m.getInt("idSenzorIncr");
+                                float vrednostSenzor = Float.valueOf(m.getString("vrednostSenzor"));
+                                String OpisNotifikacije = m.getString("OpisNotifikacije");
+
+                                grupaPodataka.put("vremeSenzor",vremeSenzor);
+                                grupaPodataka.put("idSenzorIncr", String.valueOf(idSenzorIncr));
+                                grupaPodataka.put("vrednostSenzor", String.valueOf(vrednostSenzor));
+                                grupaPodataka.put("OpisNotifikacije", OpisNotifikacije);
+
+                                arraylist.add(grupaPodataka);
+
+                            }
+
+
 
                         }
+
+                        // Get And Sent all data to ListView
+                        callListView(arraylist);
+                        Log.d("testmiki", String.valueOf(arraylist));
 
                     } else {
                         Log.d("testmiki", "Usao u ERROR");
                         // login error
-                        String errorMsg = jObj.getString("error_msg");
+                        String errorMsg = jsonObject.getString("error_msg");
                         Toast.makeText(getApplicationContext(),
                                 errorMsg, Toast.LENGTH_LONG).show();
                     }
@@ -123,7 +192,7 @@ public class SensorDetailActivity extends AppCompatActivity {
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getApplicationContext(),
                         error.getMessage(), Toast.LENGTH_LONG).show();
-                progressDialog.hideDialog();
+                // progressDialog.hideDialog();
             }
         }) {
 
@@ -151,6 +220,23 @@ public class SensorDetailActivity extends AppCompatActivity {
 
         // Adding request to  queue
         AppController.getInstance().addToRequestQueue(strReq, tag_string_req);
+    }
+
+    private void callListView(ArrayList arraylist) {
+        Log.d("testmiki", String.valueOf(arraylist));
+
+        if (arraylist.size() > 0) {
+            // Pass the results into ListViewAdapter.java
+            adapter = new ViewAdapterSensorDetail(SensorDetailActivity.this, arraylist);
+
+            // Set the adapter to the ListView
+            listView.setAdapter(adapter);
+
+        } else {
+            Toast.makeText(getApplicationContext(), "No date from sensor.", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
 }
---
+++ app/src/main/java/rs/direktnoizbaste/dizb/ViewAdapterSensorDetail.java
@@ -0,0 +1,67 @@
+package rs.direktnoizbaste.dizb;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+class ViewAdapterSensorDetail extends BaseAdapter {
+
+    String senzorTipIme = "senzorTipIme";
+
+    TextView tvKategName, tvKategLink, tvKategId,tvDaliImaPodKat,tvBrojArtikalaKateg;
+
+    private final Context context;
+    ArrayList<HashMap<String, String>> data;
+    LayoutInflater inflater;
+    HashMap<String, String> resultp = new HashMap<String, String>();
+
+    public ViewAdapterSensorDetail(Context viewAdapterSensorDetail, ArrayList<HashMap<String, String>> arraylist) {
+        this.context = viewAdapterSensorDetail;
+        data = arraylist;
+    }
+
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+        View itemView = inflater.inflate(R.layout.onerowacitvity_sensor, parent, false);
+
+        resultp = data.get(position);
+
+        tvKategName = (TextView) itemView.findViewById(R.id.tvKategName);
+        tvKategName.setTypeface(null, Typeface.BOLD);
+
+        String nazivKategorije = resultp.get(this.senzorTipIme);
+        tvKategName.setText("asdads");
+
+        return itemView;
+    }
+
+
+}
+
